4738eb7d012edb4e4c79297a0a3a7f85da62e1ce
Gato <Roy.Hagmansen@gmail.com>
2021-09-09 10:05:46 +0200

    Initial commit

---
+++ SimpleChess/Bishop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChess
+{
+    internal class Bishop : Piece
+    {
+        public Bishop() : base("LPR") { }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var col = fromPosition[0] - toPosition[0];
+            var row = fromPosition[1] - toPosition[1];
+
+            return Math.Abs(row) == Math.Abs(col);
+        }
+    }
+
+}
